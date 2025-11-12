86776170572d679108c94770a28e906074043685
Alex Quennelle <BluePhoenixCG@gmail.com>
2025-11-12 10:23:27 -0600

    Add raylib

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Raylib_cs;
+
+namespace RockPaperCheckers;
+
+internal static class Program
+{
+	[STAThread]
+	public static void Main()
+	{
+		Raylib.InitWindow(800,400,"Rock Paper Checkers");
+
+		while (!Raylib.WindowShouldClose())
+		{
+			Raylib.BeginDrawing();
+			Raylib.ClearBackground(new(100,149,237,255));
+			Raylib.EndDrawing();
+		}
+	}
+}
